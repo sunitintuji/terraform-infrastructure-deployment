e86ab3ca8cf0f583f5ed2e8b18ce11211b1a0b1a
sunitkhadgi <sunitkhadgi13@gmail.com>
2024-02-26 08:01:39 +0545

    removed comments

--- modules/lightsail_instance/main.tf
+++ modules/lightsail_instance/main.tf
@@ -32,62 +32,9 @@ resource "aws_lightsail_instance_public_ports" "public" {
       from_port = port_info.value.port
       to_port   = port_info.value.port
       cidrs     = port_info.value.cidrs
-      #cidr_list_aliases = port_info.value.cidr_list_aliases
+
 
     }
   }
 }
 
-# resource "aws_lightsail_instance_public_ports" "lightsail_instance" {
-#   instance_name = aws_lightsail_instance.lightsail_instance.name
-
-#   for_each = var.ingress
-#   port_info {
-#     protocol  = ingress.value.protocol #"tcp"
-#     from_port = ingress.value.from_port #8000
-#     to_port   = ingress.value.to_port #8000
-#     cidrs = ingress.value.cidrs
-    
-#   }
-
-
-
-#   #   dynamic "ingress" {
-#   #   for_each = var.ingress_rules
-#   #   # content {
-#   #   #   from_port   = ingress.value.from_port
-#   #   #   to_port     = ingress.value.to_port
-#   #   #   protocol    = ingress.value.protocol
-#   #   #   cidr_blocks = ingress.value.cidr_blocks
-#   #   # }
-
-#   #     port_info {
-#   #   protocol  = ingress.value.protocol #"tcp"
-#   #   from_port = ingress.value.from_port #8000
-#   #   to_port   = ingress.value.to_port #8000
-#   # }
-#   # }
-
-
-# }
-
-# resource "aws_security_group" "lightsail_security_group" {
-
-# count          = var.sg_create ? 1 : 0
-#   name        = "${var.sg_name}-${var.env}"
-
-#   dynamic "ingress" {
-#     for_each = var.ingress_rules
-#     content {
-#       from_port   = ingress.value.from_port
-#       to_port     = ingress.value.to_port
-#       protocol    = ingress.value.protocol
-#       cidr_blocks = ingress.value.cidr_blocks
-#     }
-#   }
-# }
-
-# resource "aws_network_interface_sg_attachment" "sg_attachment" {
-#   security_group_id    = aws_security_group.lightsail_security_group.id
-#   network_interface_id = aws_lightsail_instance.lightsail_instance.primary_network_interface_id
-# }
